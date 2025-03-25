3753c33bb2f361a15b5534b0ade0d6c9d230f57f
visionarchitect <visionarchitect@gmail.com>
2025-03-25 09:55:57 +0100

    Update JWTRefreshEndpoint.java

--- src/main/java/org/owasp/webgoat/lessons/jwt/JWTRefreshEndpoint.java
+++ src/main/java/org/owasp/webgoat/lessons/jwt/JWTRefreshEndpoint.java
@@ -43,7 +43,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class JWTRefreshEndpoint implements AssignmentEndpoint {
 
   public static final String PASSWORD = "bm5nhSkxCXZkKRy4";
-  private static final String JWT_PASSWORD = "bm5n3SkxCX4kKRy4";
+  private static final String JWT_PASSWORD = "bm5n3SkxCX4kKRy4"; //password
   private static final List<String> validRefreshTokens = new ArrayList<>();
 
   @PostMapping(
